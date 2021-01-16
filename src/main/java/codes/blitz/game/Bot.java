9cf9d5ce19bd01bf80b5ee907a4e4fa63668acb5
Adam Hammes <ahammes@cortexmedia.ca>
2021-01-15 19:12:54 -0500

    Successfully return to base

--- src/main/java/codes/blitz/game/Bot.java
+++ src/main/java/codes/blitz/game/Bot.java
@@ -1,5 +1,6 @@
 package codes.blitz.game;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -31,37 +32,125 @@ public class Bot {
 		var mine = this.getMinePosition();
 		final var walkTo = this.getWalkablePositionAround(mine);
 
+		// #x###########
+		// ###o##########
+		// ###o#########
+		// ###o######o##
+		// #############
+		// #############
 
 		List<Action> actions = myCrew.getUnits().stream()
-				.map(unit -> new UnitAction(UnitActionType.MOVE, unit.getId(),
-						walkTo))
+				.map(unit -> {
+					var unitPosition = unit.getPosition();
+					var adjacentPositions = getAdjacentPositions(unitPosition);
+
+
+					// if we have blitzium
+					if (unit.getBlitzium() > 4) {
+					    // if we are adjacent to a base:
+						for (var adjPos: getAdjacentPositions(unitPosition)) {
+							if (positionHasType(adjPos, TileType.BASE)) {
+								return new UnitAction(UnitActionType.DROP, unit.getId(), adjPos);
+							}
+						}
+
+						// else, move towards a depot
+					    var myBase = gameMessage.getCrewsMapById().get(myCrew.getId()).getHomeBase();
+					    var moveTo = getAdjacentPositions(myBase).get(0);
+						return new UnitAction(UnitActionType.MOVE, unit.getId(), moveTo);
+					}
+
+					// else, check if we are by a mine; if so, mine!!!
+					for (var pos: adjacentPositions) {
+						if (positionHasType(pos, TileType.MINE)) {
+							System.out.println("mine!");
+							return new UnitAction(UnitActionType.MINE, unit.getId(), pos);
+						}
+					}
+
+					// else, try to walk towards a mine
+					System.out.println("walk towards mine");
+					return new UnitAction(UnitActionType.MOVE, unit.getId(), walkTo);
+				})
 				.collect(Collectors.toList());
 
 		return actions;
 
 	}
 
-	public Position getWalkablePositionAround(Position p) {
-		var dx = new int[] { -1, 1};
-		var dy = new int[] { -1, 1};
+	public List<Position> getAllPositions() {
+	    var positions = new ArrayList<Position>();
 
-		for (var x: dx) {
-			for (var y: dy) {
-				var new_x = p.getX() + x;
-				var new_y = p.getY() + y;
+		var mapSize = this.map.getMapSize();
+		for (int x = 0; x < mapSize; x++) {
+			for (int y = 0; y < mapSize; y++) {
+				var position = new Position(x, y);
 
-				var position = new Position(new_x, new_y);
+
 
 				try {
-					if (map.getTileTypeAt(position) == TileType.EMPTY) {
-						return position;
-					}
-
+					map.validateTileExists(position);
+					positions.add(position);
 				} catch (PositionOutOfMapException e) {}
 			}
 		}
 
+		return positions;
+	}
+
+	public boolean positionHasType(Position p, TileType t) {
+		try {
+			return map.getTileTypeAt(p) == t;
+		} catch (PositionOutOfMapException e) {
+			return false;
+		}
+	}
+
+	// Return adjacent positions to `p`. Skips positions that are off the map.
+	public List<Position> getAdjacentPositions(Position p) {
+		var positions = new ArrayList<Position>();
+
+		positions.add(new Position(p.getX() - 1, p.getY()));
+		positions.add(new Position(p.getX() + 1, p.getY()));
+		positions.add(new Position(p.getX(), p.getY() - 1));
+		positions.add(new Position(p.getX(), p.getY() + 1));
+
+		var reachablePositions = new ArrayList<Position>();
+		for (var pos: positions) {
+			try {
+				map.validateTileExists(pos);
+				reachablePositions.add(pos);
+
+			} catch (PositionOutOfMapException e) {}
+		}
+
+		return reachablePositions;
+	}
+
+	public Position getWalkablePositionAround(Position p) {
+	    var adjacentPositions = getAdjacentPositions(p);
+	    for (var adjP: adjacentPositions) {
+	    	try {
+				if (map.getTileTypeAt(adjP) == TileType.EMPTY) {
+					return adjP;
+				}
+
+			} catch (PositionOutOfMapException e) {}
+		}
 		return new Position(0, 0);// todo: find a better way of handling no walkable tiles;
+	}
+
+	public boolean canMine(Position p) {
+		var adjacentPositions = getAdjacentPositions(p);
+		for (var adjP: adjacentPositions) {
+			try {
+				if (map.getTileTypeAt(adjP) == TileType.MINE) {
+					return true;
+				}
+
+			} catch (PositionOutOfMapException e) {}
+		}
+		return false;
 	}
 
 	public Position getMinePosition() {
